fa297a0805c379018370d5c543169b60a20352fd
Avery Burne <burneav@gmail.com>
2021-11-17 14:15:22 -0500

    Imported Button and Container components into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState, useEffect} from 'react';
+import Words from './components/Words.js'
+import Container from './components/Container.js'
 import './App.css';
 
 function App() {
+  const [ word, setWord ] = useState(Words)
+  const [ newWord, setNewWord ] = useState(word[0])
+  const [ disabled, setDisabled ] = userState(true)
+  const [ currentResults, setCurrentResults ] = useState([])
+  const [ wrontResults, setWrongResults ] = useState([])
+  const [ correctCount, setCorrectCount ] = useState([])
+  const [ time, setTime ] = useState(30)
+  const [ inputValue, setInputValue ] = useState('')
+  const [ animation, setAnimation ] = userState(null)
+
+  let randomWord = Math.floor(Math.random() * word.length)
+
   return (
     <div className="App">
-      <h1>Typing Game</h1>
+      <Container></Container>
     </div>
   );
 }
